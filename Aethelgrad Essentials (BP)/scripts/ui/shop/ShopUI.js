9c4206740da37622314a35b05bcbaf596f2753a7
Wladyslaw18 <Adolfus73@outlook.com>
2026-05-04 22:08:10 +0200

    Add Aethelgrad Essentials (BP)/node_modules/@nodelib/fs.scandir/out/providers/common.d.ts

--- Aethelgrad Essentials (BP)/scripts/ui/shop/ShopUI.js
+++
@@ -1,9 +0,0 @@
-/**
- * Shop UI - Main entry point for the new shop system
- */
-
-import { showCategoryUI } from "./ShopCategoryUI.js"
-
-export async function showShopUI(player) {
-    await showCategoryUI(player)
-}
