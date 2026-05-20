6f57f3a415bfcf23def91ff5e19aa328a94fcd94
WladyslawKW <adolfus73@outlook.com>
2026-05-20 04:49:59 +0200

    refactor(admin): update AdminPanelBanned UI logic

--- scripts/commands/admin/AdminPanelBanned.js
+++ scripts/commands/admin/AdminPanelBanned.js
@@ -9,7 +9,7 @@ import { UIUtils } from "../../ui/UIUtils.js"
 export async function showBannedPlayers(player) {
     const PermissionManager = Kernel.get("permissions")
     if (!PermissionManager.hasPermission(player, "essentials.admin")) {
-        player.sendMessage("\xA7cNo permission.")
+        player.sendMessage("\u00A7cNo permission.")
         return
     }
 
@@ -17,14 +17,14 @@ export async function showBannedPlayers(player) {
     const bans = BanManager.getBans()
     
     const form = new Kernel.ActionFormData()
-        .title("\xA7a\xA7e\xA7l\xA7c\xA7lBanned Panel")
-        .body(`\xA7aTotal Players Banned : \xA7f${bans.length}`)
+        .title("\u00A7a\u00A7e\u00A7l\u00A7c\u00A7lBanned Panel")
+        .body(`\u00A7aTotal Players Banned : \u00A7f${bans.length}`)
 
     bans.forEach(ban => {
-        form.button(`\xA7c\xA7l${ban.playerName}`, "textures/items/iron_axe")
+        form.button(`\u00A7c\u00A7l${ban.playerName}`, "textures/items/iron_axe")
     })
 
-    form.button("\xA7c<= BACK", "textures/ui/refresh")
+    form.button("\u00A7c<= BACK", "textures/ui/refresh")
 
     const res = await UIUtils.showForm(player, form)
     if (res.canceled) return
@@ -42,10 +42,10 @@ async function showBanInfoPanel(player, ban) {
     const expires = ban.expires === 0 ? "PERMANENT" : new Date(ban.expires).toLocaleString()
     
     const form = new Kernel.ActionFormData()
-        .title(`\xA7c\xA7l${ban.playerName} Ban`)
-        .body(`\xA7aId : \xA7f${ban.playerId}\n\xA7aReason : \xA7f${ban.reason}\n\xA7aExpires : \xA7f${expires}`)
-        .button("\xA7eUnban Player", "textures/items/totem")
-        .button("\xA7c<= BACK", "textures/ui/refresh")
+        .title(`\u00A7c\u00A7l${ban.playerName} Ban`)
+        .body(`\u00A7aId : \u00A7f${ban.playerId}\n\u00A7aReason : \u00A7f${ban.reason}\n\u00A7aExpires : \u00A7f${expires}`)
+        .button("\u00A7eUnban Player", "textures/items/totem")
+        .button("\u00A7c<= BACK", "textures/ui/refresh")
 
     const res = await UIUtils.showForm(player, form)
     if (res.canceled) return
@@ -58,7 +58,7 @@ async function showBanInfoPanel(player, ban) {
         const newBans = bans.filter(b => b.playerId !== ban.playerId)
         WorldStore.set("ae:bans", newBans)
         
-        player.sendMessage(`\xA7a\xA7l» \xA7fSuccessfully unbanned \xA7e${ban.playerName}\xA7f.`)
+        player.sendMessage(`\u00A7a\u00A7l» \u00A7fSuccessfully unbanned \u00A7e${ban.playerName}\u00A7f.`)
         await showBannedPlayers(player)
     } else {
         await showBannedPlayers(player)
