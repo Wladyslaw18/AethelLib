5f94b5d63805930b47090234b84657dacf5609c7
WladyslawKW <adolfus73@outlook.com>
2026-06-05 20:03:30 +0200

    chore(plugins): disable old BountyHunter plugin from main registry

--- scripts/plugins/PluginLoader.js
+++ scripts/plugins/PluginLoader.js
@@ -2,7 +2,6 @@
 // yes, it's hardcoded. no, we can't use fs.readdir in bedrock. deal with it.
 export const pluginDefs = [
     { path: "CoreEconomy", loader: () => import("./CoreEconomy/index.js") },
-    { path: "BountyHunter", loader: () => import("./BountyHunter/index.js") },
     { path: "Clans", loader: () => import("./Clans/index.js") },
     { path: "AethelEssentials", loader: () => import("./AethelEssentials/index.js") }
 ];
